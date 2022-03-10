493280f5057a0db46f395bb2208dcd693e3fbd4d
Rhys Compton <rhys.compton@gmail.com>
2022-03-10 16:20:03 -0500

    :bug: Fix GPU jupyter sbatch script

--- start_singularity.sh
+++ start_singularity.sh
@@ -1,9 +1,10 @@
 echo "Starting up Singularity container..."
 
-singularity shell --nv \
+singularity exec --nv \
 	--overlay /scratch/wz727/chestXR/data/mimic-cxr.sqsh:ro \
 	--overlay /scratch/lhz209/data/chexpert.sqf:ro \
 	--overlay /scratch/lhz209/data/padchest.sqf:ro \
 	--overlay /scratch/wz727/chestXR/data/chestxray8.sqsh  \
 	--overlay /scratch/lhz209/pytorch1.7.0-cuda11.0.ext3:ro \
-	/scratch/work/public/singularity/centos-8.2.2004.sif
+	/scratch/work/public/singularity/cuda11.3.0-cudnn8-devel-ubuntu20.04.sif \
+	/bin/bash
